2560d1c396747dac5cabd1e0234dcf64aa0b1095
matthew.bass <matthew.bass@discordapp.com>
2024-10-21 14:32:26 -0500

    comment stuff out

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-# clone plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#
-# append contents (don't clobber clyde)
-cat zshrc_append >> ~/.zshrc
+ #!/bin/sh
+# 
+# # install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+# 
+# # clone plugins
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# 
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# #
+# # append contents (don't clobber clyde)
+# cat zshrc_append >> ~/.zshrc
